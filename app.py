d6c27c780b8d7292bdd2bdf562666dc5920ca337
Carlos Roca <cr5394@hotmail.com>
2021-07-21 17:09:04 -0400

    Changes to auth

--- app.py
+++ app.py
@@ -1,27 +1,21 @@
 import os
 import cv2
 
-
-# os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-# os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
-
 from flask import Flask, redirect, url_for, render_template, Response
 from flask_dance.contrib.google import make_google_blueprint, google
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecret'
-
-blueprint = make_google_blueprint(client_id='384816377604-dhu0r6k2cg2ojfsonvbuqa2tcrkjdkkd.apps.googleusercontent.com', 
-client_secret='5wspr8oyOAKYmy7d15Pc2gQK',
-offline=True, scope=['profile', 'email'])
-
-app.register_blueprint(blueprint,url_prefix='/login')
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
+app.config["GOOGLE_OAUTH_CLIENT_ID"] = '384816377604-dhu0r6k2cg2ojfsonvbuqa2tcrkjdkkd.apps.googleusercontent.com'
+app.config["GOOGLE_OAUTH_CLIENT_SECRET"] = '5wspr8oyOAKYmy7d15Pc2gQK'
+google_bp = make_google_blueprint(scope=["profile", "email"])
+app.register_blueprint(google_bp, url_prefix="/login")
 
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('base.html')
 
 def generate():
     vs = cv2.VideoCapture(0)
@@ -41,17 +35,12 @@ def video_feed():
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
-
-@app.route('/login')
-def login():
     if not google.authorized:
         return redirect(url_for("google.login"))
-    resp = google.get("/oauth2/v2/userinfo")
+    resp = google.get("/oauth2/v1/userinfo")
     assert resp.ok, resp.text
-    name = resp.json()['name']
-
-    return redirect(url_for('home'))
+    name = resp.json()["name"]
+    return render_template('home.html', name=name)
 
 
 if __name__ == "__main__":
