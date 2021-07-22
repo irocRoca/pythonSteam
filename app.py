bc092f2caf9f7cb72a79e1ddca537fc9d5677900
Carlos Roca <cr5394@hotmail.com>
2021-07-21 23:27:08 -0400

    Oauth and changes:

--- app.py
+++ app.py
@@ -1,45 +1,66 @@
 import os
-import cv2
-from flask import Flask, redirect, url_for, Response, render_template
-from flask_dance.contrib.github import make_github_blueprint, github
+# setting the environment variables for local testing / not used on deployment
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+
+from flask import Flask, redirect, url_for, render_template
+from flask_dance.contrib.google import make_google_blueprint, google
 
 app = Flask(__name__)
-app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
-app.config["GITHUB_OAUTH_CLIENT_ID"] = os.environ.get("GITHUB_OAUTH_CLIENT_ID")
-app.config["GITHUB_OAUTH_CLIENT_SECRET"] = os.environ.get("GITHUB_OAUTH_CLIENT_SECRET")
-github_bp = make_github_blueprint()
-app.register_blueprint(github_bp, url_prefix="/login")
 
-@app.route("/")
+# setting up the environment variables
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = os.environ.get('OAUTHLIB_RELAX_TOKEN_SCOPE')
+app.config['SECRET_KEY'] = 'mysecret'
+app.config['GOOGLE_OAUTH_CLIENT_ID'] = os.environ.get('CLIENT_ID')
+app.config['GOOGLE_OAUTH_CLIENT_SECRET'] = os.environ.get('CLIENT_SECRET')
+
+# setting up the google blueprint & registring it
+blueprint = make_google_blueprint(reprompt_consent=True, scope=['profile', 'email'])
+app.register_blueprint(blueprint, url_prefix='/login')
+
+
+@app.route('/')
 def index():
-    return render_template('base.html')
+    if google.authorized:
+        resp = google.get('/oauth2/v3/userinfo')
+        assert resp.ok, resp.text
+        name = resp.json()['name']
+        return render_template('home.html', name=name)
+    else:
+        return render_template('base.html')
 
-@app.route("/home")
+
+@app.route('/static_feed')
+def static_feed():
+    if google.authorized:
+        resp = google.get('/oauth2/v3/userinfo')
+        assert resp.ok, resp.text
+        name = resp.json()['name']
+        return render_template('static.html', name=name)
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route('/live_feed')
+def live_feed():
+    if google.authorized:
+        resp = google.get('/oauth2/v3/userinfo')
+        assert resp.ok, resp.text
+        name = resp.json()['name']
+        return render_template('live_feed.html', name=name)
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/login/google')
 def login():
-    if not github.authorized:
-        return redirect(url_for("github.login"))
-    resp = github.get("/user")
-    assert resp.ok
-    return render_template('home.html', login=resp.json()["login"])
+    if not google.authorized:
+        return render_template(url_for('google.login'))
+    resp = google.get('/oauth2/v3/userinfo')
+    assert resp.ok, resp.text
+    name = resp.json()['name']
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate(), mimetype = "multipart/x-mixed-replace; boundary=frame")
-
-def generate():
-    vs = cv2.VideoCapture(0)
-    while True:
-
-        ret, frame = vs.read()
-        (flag, encodedImage) = cv2.imencode('.jpg', frame)
-
-        if not flag:
-            continue
-        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
-			bytearray(encodedImage) + b'\r\n')
+    return redirect('home.html', name=name)
 
 
-
-
-if __name__ == "__main__":
-    app.run(threading=True)
+if __name__ == '__main__':
+    app.run(debug=True)
