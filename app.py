313709ee498924e91d2481ae1ce8c48cbe9ecbfc
Carlos Roca <cr5394@hotmail.com>
2021-07-21 21:24:37 -0400

    modifications

--- app.py
+++ app.py
@@ -1,27 +1,30 @@
 import os
 import cv2
-
-
-from flask import Flask, redirect, url_for, render_template, Response
+from flask import Flask, redirect, url_for, Response, render_template
 from flask_dance.contrib.google import make_google_blueprint, google
-
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
-app.config["GOOGLE_OAUTH_CLIENT_ID"] = '384816377604-dhu0r6k2cg2ojfsonvbuqa2tcrkjdkkd.apps.googleusercontent.com'
-app.config["GOOGLE_OAUTH_CLIENT_SECRET"] = '5wspr8oyOAKYmy7d15Pc2gQK'
+app.config["GOOGLE_OAUTH_CLIENT_ID"] = os.environ.get("GOOGLE_OAUTH_CLIENT_ID")
+app.config["GOOGLE_OAUTH_CLIENT_SECRET"] = os.environ.get("GOOGLE_OAUTH_CLIENT_SECRET")
 google_bp = make_google_blueprint(scope=["profile", "email"])
 app.register_blueprint(google_bp, url_prefix="/login")
 
+@app.route("/")
+def index():
+    return render_template('base.html')
 
-@app.route('/')
-def home():
+@app.route("/home")
+def login():
     if not google.authorized:
         return redirect(url_for("google.login"))
     resp = google.get("/oauth2/v1/userinfo")
     assert resp.ok, resp.text
-    name = resp.json()["name"]
-    return render_template('home.html', name=name)
+    return render_template('home.html', name=resp.json()["name"])
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate(), mimetype = "multipart/x-mixed-replace; boundary=frame")
 
 def generate():
     vs = cv2.VideoCapture(0)
@@ -35,9 +38,7 @@ def generate():
         yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
 			bytearray(encodedImage) + b'\r\n')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate(), mimetype = "multipart/x-mixed-replace; boundary=frame")
+
 
 
 if __name__ == "__main__":
